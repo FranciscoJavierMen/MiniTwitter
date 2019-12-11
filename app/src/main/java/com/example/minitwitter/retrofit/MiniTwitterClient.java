638b27df67c89261f2d6718de36fdad79634d2cd
Francisco Javier Mendoza <pacopancho413@gmail.com>
2019-12-11 10:02:51 -0600

    Adding access to internet

--- app/src/main/java/com/example/minitwitter/retrofit/MiniTwitterClient.java
+++ app/src/main/java/com/example/minitwitter/retrofit/MiniTwitterClient.java
@@ -2,6 +2,14 @@ package com.example.minitwitter.retrofit;
 
 import com.example.minitwitter.common.Constantes;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -12,9 +20,30 @@ public class MiniTwitterClient {
     private Retrofit retrofit;
 
     public MiniTwitterClient(){
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(chain -> {
+                    Request original = chain.request();
+                    Request request = original.newBuilder()
+                            .header("Content-Type","application/json")
+                            .header("Accept","application/json")
+                            .method(original.method(),original.body())
+                            .build();
+
+                    return chain.proceed(request);
+                }).addInterceptor(interceptor)
+                .callTimeout(30, TimeUnit.SECONDS)
+                .readTimeout(30,TimeUnit.SECONDS)
+                .connectTimeout(30, TimeUnit.SECONDS)
+                .writeTimeout(30, TimeUnit.SECONDS)
+                .build();
+
         retrofit = new Retrofit.Builder()
                 .baseUrl(Constantes.API_MINITWITTER_BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
                 .build();
 
         miniTwitterService = retrofit.create(MiniTwitterService.class);
